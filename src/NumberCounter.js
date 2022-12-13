c8394bfcf9184e01f2371bcc6d5265f9c186afaa
Bomi-kwon <bbomicoding@gmail.com>
2022-12-13 21:33:11 +0900

    삼항연산자에서 && 쓰기

--- src/NumberCounter.js
+++ src/NumberCounter.js
@@ -3,16 +3,17 @@ import React, { useState } from "react";
 function NumberCounter() {
   const [no, setNo] = useState(0);
 
-  const noIsEvenDiv =
-    no % 2 == 0 ? (
-      <>
-        <span>짝수입니다.</span>
-      </>
-    ) : (
-      <>
-        <span>홀수입니다.</span>
-      </>
-    );
+  const noIsEvenDiv = (
+    <>
+      <hr /> {no % 2 == 0 ? <span>짝수입니다.</span> : <span>홀수입니다.</span>}
+    </>
+  );
+
+  const noIs8multiple = no % 8 == 0 && (
+    <>
+      <span>8의 배수입니다.</span>
+    </>
+  );
 
   return (
     <>
@@ -22,13 +23,7 @@ function NumberCounter() {
       <hr />
       {noIsEvenDiv}
       <hr />
-      {no % 8 == 0 ? (
-        <>
-          <span>8의 배수입니다.</span>
-        </>
-      ) : (
-        <></>
-      )}
+      {noIs8multiple}
     </>
   );
 }
