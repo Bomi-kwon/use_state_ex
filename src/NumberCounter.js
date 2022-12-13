e0886858343c3df173baac0b43c8bb24f36878e3
Bomi-kwon <bbomicoding@gmail.com>
2022-12-13 21:14:04 +0900

    버튼 누르면 숫자 커지고 짝수인지 판별하기(삼항연산자)

---
+++ src/NumberCounter.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function NumberCounter() {
+  const [no, setNo] = useState(0);
+
+  const noIsEvenDiv =
+    no % 2 == 0 ? (
+      <>
+        <span>짝수입니다.</span>
+      </>
+    ) : (
+      <></>
+    );
+
+  return (
+    <>
+      숫자:{no}
+      <hr />
+      <button onClick={() => setNo(no + 1)}>증가</button>
+      <hr />
+      {noIsEvenDiv}
+    </>
+  );
+}
+
+export default NumberCounter;
